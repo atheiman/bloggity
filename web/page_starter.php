63651eabbe9193a59e268e329c90e22aec4beb3c
Austin Heiman <atheiman@ksu.edu>
2014-04-10 17:39:38 -0500

    styling work, drastic improvements

--- web/page_starter.php
+++ web/page_starter.php
@@ -1,25 +1,28 @@
 <?php
 session_start();
+
 // Testing
-$_SESSION["userType"] = "Administrator";
+session_destroy();
+//$_SESSION['userType'] = 'Administrator';
+//$_SESSION['userType'] = 'Editor';
 
 // Secure Page
-//if (!isset($_SESSION["userType"])) {header("Location: login.php");}
+//if (!isset($_SESSION['userType'])) {header('Location: login.php');}
 
 // Admin Only Page
-//if ($_SESSION["userType"] != "Administrator") {die("This page is restricted to Adminstrators");}
+//if ($_SESSION['userType'] != 'Administrator') {die('This page is restricted to Adminstrators');}
 
 // MySQL connection
-include "./resources/db_connect.php";
+include './resources/db_connect.php';
 
 ?>
 <!doctype html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="./resources/style.css">
-<script src="./resources/script.js"></script>
+<link rel='stylesheet' type='text/css' href='./resources/style.css'>
+<script src='./resources/script.js'></script>
 <title>Test Bloggity</title>
-<link rel="icon" type="image/png" href="files/images/generic_icon.png"/>
+<link rel='icon' type='image/png' href='files/images/generic_icon.png'/>
 <style>
 
 </style>
@@ -29,7 +32,7 @@ include "./resources/db_connect.php";
 </head>
 
 <body onload="">
-<?php include "./resources/header.php" ; ?>
+<?php include './resources/header.php' ; ?>
 <!-- #page_content started -->
 Content of Page
 
--- web/resources/header.php
+++ web/resources/header.php
@@ -6,10 +6,17 @@
 <?php
 if (isset($_SESSION['userType'])) {
   echo "
-  <div id='admin_bar' style='width:150px;' class='gray_bg center'>
-    <a href='admin_link1.php'>Link1</a>
-    <a href='admin_link2.php'>Link2</a>
-    <a href='admin_link3.php'>Link3</a>
+  <div id='admin_bar' style='width:150px;' class='center'>
+    <a href='admin_link1.php'>Posts</a>
+    <a href='admin_link2.php'>Pages</a>
+    <a href='admin_link3.php'>Style</a>
+    ";
+    if ($_SESSION['userType'] == 'Administrator') {
+      echo "
+      <a href='admin_link4.php'>Users</a>
+      ";
+    }
+  echo "
   </div>
   <div id='page_content' style='width:850px;'>
   ";
